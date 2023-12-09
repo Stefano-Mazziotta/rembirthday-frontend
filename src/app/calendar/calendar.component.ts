6136201ea699f428ee03a1a735509718461e3d9c
stefano-mazziotta <mazziottastefano@gmail.com>
2023-12-09 17:24:08 -0300

    dev - working in dates logic

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarService } from './services/calendar.service';
 
+type currentMonth = {
+  name: string;
+  number: number;
+  year: number;
+  dates: Date[];
+};
+
 @Component({
   selector: 'app-calendar',
   standalone: true,
@@ -11,8 +18,34 @@ import { CalendarService } from './services/calendar.service';
 export class CalendarComponent implements OnInit {
   celebrants: any[] = [];
 
+  today: Date = new Date();
+  currentMonth: currentMonth = {
+    name: '',
+    number: 0,
+    year: 0,
+    dates: [],
+  };
+
+  futureDates: Date[] = [];
+
   constructor(private _calendarService: CalendarService) {}
+
   ngOnInit(): void {
+    const { currentMonth, today } = this;
+
+    currentMonth.name = today.toLocaleString('default', { month: 'long' });
+    currentMonth.number = today.getMonth() + 1; // getMonth -> 0 - 11 ... 0 = january...
+    currentMonth.year = today.getFullYear();
+    currentMonth.dates = this.getMonthDates(
+      currentMonth.year,
+      currentMonth.number
+    );
+
+    this.futureDates = this.getFutureDates();
+
+    // console.log(this.futureDates);
+    console.log(currentMonth);
+
     this._calendarService.getCelebrants().subscribe({
       next: (response) => {
         const { message, success, data } = response;
@@ -24,5 +57,34 @@ export class CalendarComponent implements OnInit {
         console.error(error);
       },
     });
+  }
+
+  private getFutureDates(): Date[] {
+    const futureDates: Date[] = [];
+
+    for (let i = 1; i <= 30; i++) {
+      const nextDate = new Date(this.today);
+      nextDate.setDate(this.today.getDate() + i);
+      futureDates.push(nextDate);
+    }
+
+    return futureDates;
+  }
+
+  private getMonthDates(year: number, month: number): Date[] {
+    const monthDates: Date[] = [];
+
+    const daysInCurrentMonth: number = this.getDaysInMonth(year, month);
+
+    for (let index = 1; index <= daysInCurrentMonth; index++) {
+      const date = new Date(`${year}/${month}/${index}`);
+      monthDates.push(date);
+    }
+
+    return monthDates;
+  }
+
+  private getDaysInMonth(year: number, month: number): number {
+    return new Date(year, month, 0).getDate();
   }
 }

1e665b46388337d72db1778960cceff9c3b88028
stefano-mazziotta <mazziottastefano@gmail.com>
2023-12-31 18:22:42 -0300

    dev - create constants file

---
+++ src/shared/AppConstants.ts
@@ -0,0 +1,24 @@
+export const MONTHS: string[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const WEEK_DAYS: string[] = [
+  'Sunday',
+  'Monday',
+  'Thuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
